6884571e8d8df18194484382dc842eb6379aadf7
Klaasrozema <k.rozema@st.hanze.nl>
2023-06-21 11:50:18 +0200

    Eerste opzet

---
+++ git.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#installeren van Git, en ophalen van juiste Repo
+apt-get install git
+git clone https://github.com/Klaasrozema/Linux-Services.git
---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#Ophalen en updaten van services
+apt-get update
+apt-get upgrade
