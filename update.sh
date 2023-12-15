34ade5a7e9df111abecbf6689a819b4f62fd2757
Klaasrozema <k.rozema@st.hanze.nl>
2023-12-15 22:21:14 +0100

    Uitwerking bijwerken, update toegevoegd voor snellle refresh repo

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#	--------------------
+#	File: update.sh
+#	Author: Klaas Rozema
+#	--------------------
+
+# Verwijder oude en installeer nieuwe versie van Github repo.
+
+	sudo rm -rf Linux-Services 
+	sudo git clone https://github.com/Klaasrozema/Linux-Services
